a4da318b67e237e918063eae83e861a61d5933c9
Kanth Reddy <kanth.reddyb032@gmail.com>
2020-02-21 10:16:52 +0530

    TodoApp Component added

--- app/app.jsx
+++ app/app.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-
+var TodoApp=require('TodoApp');
 
 //load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
@@ -11,6 +11,6 @@ $(document).foundation();
 //add class
 require('style!css!sass!applicationStyles');
 ReactDOM.render(
-  <p>Welcome to boilerplate 2 </p>,
+  <TodoApp/>,
   document.getElementById('app')
 );
